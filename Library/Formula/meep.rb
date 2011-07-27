6004010eb7bc9523de83f9d9481b33aa6d0665ca
Elson Liu <eyliu.dev@umich.edu>
2011-07-27 11:48:28 -0400

    MEEP formula
    MIT Electromagnetic Equation Propagation (MEEP) is a free finite-difference time-domain (FDTD) simulation software package developed at MIT to model electromagnetic systems.

---
+++ Library/Formula/meep.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Meep < Formula
+  url 'http://ab-initio.mit.edu/meep/meep-1.1.1.tar.gz'
+  homepage 'http://ab-initio.mit.edu/wiki/index.php/Meep'
+  md5 '415e0cd312b6caa22b5dd612490e1ccf'
+
+  depends_on 'harminv'
+  depends_on 'libctl'
+  depends_on 'hdf5'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
