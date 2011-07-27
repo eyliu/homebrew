e7080c60707ca4af86c010f5543408896570b178
Elson Liu <eyliu.dev@umich.edu>
2011-07-27 11:22:07 -0400

    h5utils formula
    h5utils is a set of utilities for visualization and conversion of scientific data in the free, portable HDF5 format.  Besides providing a simple tool for batch visualization as PNG images, h5utils also includes programs to convert HDF5 datasets into the formats required by other free visualization software (e.g. plain text, Vis5d, and VTK).

---
+++ Library/Formula/h5utils.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class H5utils < Formula
+  url 'http://ab-initio.mit.edu/h5utils/h5utils-1.12.1.tar.gz'
+  homepage 'http://ab-initio.mit.edu/wiki/index.php/H5utils'
+  md5 '46a6869fee6e6bf87fbba9ab8a99930e'
+
+  depends_on 'hdf5'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
